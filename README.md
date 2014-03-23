5a7c23a1454636ca8fe927772a10ef6a92ace85c
Niko Kiuru <niko@kiuru.net>
2014-03-23 15:04:59 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-Trello task taken time
+Trello tasks taken time
 ======================
 
 Each Trello tasks taken time (One board and one list)
 
-== Deploy
+## Deploy
 
 create trello_token.rb
 
 	TRELLO_DEVELOPER_PUBLIC_KEY = "your_developer_key"
 	TRELLO_MEMBER_TOKEN = "member_token"
+
+create trello_conf.rb
+
+	MEMBER_NAME = "member_name" # whom task you would like to see
+	BOARD_ID = "board_id" # member some board id which tasks times you would like to follow
--- client.rb
+++ client.rb
@@ -1,17 +1,18 @@
 require 'trello'
 require './trello_token.rb'
-
-member_name = "kiuru" # whom task you would like to see
+require './trello_conf.rb'
 
 Trello.configure do |config|
   config.developer_public_key = TRELLO_DEVELOPER_PUBLIC_KEY
   config.member_token = TRELLO_MEMBER_TOKEN
 end
 
-member = Trello::Member.find(member_name)
+member = Trello::Member.find(MEMBER_NAME)
 puts "#{member.full_name}"
 puts "Bio: #{member.bio}"
 
-member.boards.each do |board|
-	puts board.name
+board = Trello::Board.find(BOARD_ID)
+
+board.actions.each do |action|
+	puts "id: #{action.id}, type: #{action.type}"
 end
